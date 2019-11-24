0372b89f30a28b3fb0ed489bc0938da500b35ef9
wangrq <98709484@qq.com>
2019-11-24 17:56:22 +0800

    fix: 3.1

--- 2019-12-04-Java/Assignment3/Assignment3/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
+++ 2019-12-04-Java/Assignment3/Assignment3/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
@@ -7,28 +7,28 @@ import javafx.scene.layout.GridPane;
 
 public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEvent> {
 
-	private View view; // So we can talk to our parent or other components of the view
+    private View view; // So we can talk to our parent or other components of the view
 
-	public ShapeChooserPanel(View view) {
+    public ShapeChooserPanel(View view) {
 
-		this.view = view;
+        this.view = view;
 
-		String[] buttonLabels = {"Circle", "Rectangle", "Squiggle", "Polyline"};
+        String[] buttonLabels = {"Circle", "Rectangle", "Squiggle", "Polyline"};
 
-		int row = 0;
-		for (String label : buttonLabels) {
-			Button button = new Button(label);
-			button.setMinWidth(100);
-			this.add(button, 0, row);
-			row++;
-			button.setOnAction(this);
-		}
-	}
+        int row = 0;
+        for (String label : buttonLabels) {
+            Button button = new Button(label);
+            button.setMinWidth(100);
+            this.add(button, 0, row);
+            row++;
+            button.setOnAction(this);
+        }
+    }
 
-	@Override
-	public void handle(ActionEvent event) {
-		String command = ((Button) event.getSource()).getText();
-		this.view.setPaintPanelShapeManipulatorStrategy(ShapeManipulatorFactory.create(command, view.getPaintModel()));
-		System.out.println(command);
-	}
+    @Override
+    public void handle(ActionEvent event) {
+        String command = ((Button) event.getSource()).getText();
+        this.view.setPaintPanelShapeManipulatorStrategy(ShapeManipulatorFactory.create(command, view.getPaintModel()));
+        System.out.println(command);
+    }
 }
