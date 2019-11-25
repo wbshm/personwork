804d1895b331254f4579fce9192411a2b7f7ffe7
wangrq <98709484@qq.com>
2019-11-25 12:38:16 +0800

    fix: 3.3

--- 2019-12-04-Java/Assignment3/Assignment3/src/ca/utoronto/utm/paint/PaintFileParser.java
+++ 2019-12-04-Java/Assignment3/Assignment3/src/ca/utoronto/utm/paint/PaintFileParser.java
@@ -35,6 +35,11 @@ public class PaintFileParser {
     private static final int POLYLINE = 5;
     private static final int ENDFILE = 6;
 
+    private String nextOrder = "color";
+    private static final String colorStr = "color";
+    private static final String fillStr = "filled";
+    private static final String endStr = "end";
+
     /**
      * Below are Patterns used in parsing
      */
@@ -107,6 +112,7 @@ public class PaintFileParser {
         // following commands. As we parse the file, we modify
         // the appropriate command.
         PaintCommand paintCommand = null;
+        ArrayList<PaintCommand> commands = new ArrayList<>();
 
         try {
             int state = 0;
@@ -127,7 +133,7 @@ public class PaintFileParser {
                             break;
                         }
                         error("Expected Start of Paint Save File");
-                        return false;
+                        res = -1;
                     case 1:
                         state = -1;
                         if (pCircleStart.matcher(l).matches()) {
@@ -144,6 +150,8 @@ public class PaintFileParser {
                             state = POLYLINE;
                         } else if (pFileEnd.matcher(l).matches()) {
                             state = ENDFILE;
+                        } else if ("".equals(l)) {
+                            state = 1;
                         }
                         break;
                     case CIRCLE:
@@ -159,22 +167,33 @@ public class PaintFileParser {
                         res = parsePolyline(l, (PolylineCommand) paintCommand);
                         break;
                     case ENDFILE:
-                        res = 1;
+                        if ("".equals(l)) {
+                            res = 1;
+                        } else {
+                            error("File end is wrong");
+                            res = -1;
+                        }
                         break;
                     default:
                         error("Nothing matched");
+                        res = -1;
                         break;
                 }
                 if (res == 0) {
-                    this.paintModel.addCommand(paintCommand);
+                    commands.add(paintCommand);
+                    res = 1;
                     state = 1;
                 } else if (res == -1) {
                     //It's already gone wrong
+                    System.out.println(getErrorMessage());
                     return false;
                 }
             }
         } catch (Exception e) {
-
+            System.out.println("Fatal error");
+        }
+        for (PaintCommand paint : commands) {
+            this.paintModel.addCommand(paint);
         }
         return true;
     }
@@ -187,43 +206,57 @@ public class PaintFileParser {
      * @return int 0 exist , -1 error , 1 continue
      */
     private int parseCircle(String str, CircleCommand circle) {
-        Matcher m;
+        final String centerStr = "center";
+        final String radiusStr = "radius";
         int res = 1;
-        if (str.contains("color:")) {
-            Color color = getColor(str);
-            if (color == null) {
-                error("Failed to parse circle color");
+        Matcher m;
+        switch (nextOrder) {
+            case colorStr:
+                res = setColor(str, circle);
+                break;
+            case fillStr:
+                m = pFill.matcher(str);
+                if (m.find()) {
+                    circle.setFill("true".equals(m.group(1)));
+                } else {
+                    error("Failed to parse circle filled");
+                    res = -1;
+                }
+                break;
+            case centerStr:
+                m = pPoint.matcher(str);
+                if (m.find()) {
+                    Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
+                    circle.setCentre(point);
+                } else {
+                    error("Failed to parse circle centre");
+                    res = -1;
+                }
+                break;
+            case radiusStr:
+                m = pCircleRadius.matcher(str);
+                if (m.find()) {
+                    circle.setRadius(Integer.parseInt(m.group(1)));
+                } else {
+                    error("Failed to parse circle radius");
+                    res = -1;
+                }
+                break;
+            case endStr:
+                m = pCircleEnd.matcher(str);
+                res = 0;
+                if (!m.matches()) {
+                    error("Failed to parse circle end");
+                    res = -1;
+                }
+                break;
+            default:
+                error("Failed to parse circle");
                 res = -1;
-            }
-            circle.setColor(color);
-        } else if (str.contains("filled:")) {
-            m = pFill.matcher(str);
-            if (m.find()) {
-                circle.setFill("true".equals(m.group(1)));
-            } else {
-                error("Failed to parse circle filled");
-                res = -1;
-            }
-        } else if (str.contains("center:")) {
-            m = pPoint.matcher(str);
-            if (m.find()) {
-                Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
-                circle.setCentre(point);
-            } else {
-                error("Failed to parse circle centre");
-                res = -1;
-            }
-        } else if (str.contains("radius:")) {
-            m = pCircleRadius.matcher(str);
-            if (m.find()) {
-                circle.setRadius(Integer.parseInt(m.group(1)));
-            } else {
-                error("Failed to parse circle radius");
-                res = -1;
-            }
-        } else {
-            m = pCircleEnd.matcher(str);
-            res = m.matches() ? 0 : -1;
+        }
+        if (res != -1) {
+            String[] strArr = {colorStr, fillStr, centerStr, radiusStr, endStr};
+            setNextOrder(strArr);
         }
         return res;
     }
@@ -238,152 +271,204 @@ public class PaintFileParser {
     private int parseRectangle(String str, RectangleCommand rectangle) {
         int res = 1;
         Matcher m;
-        if (str.contains("color:")) {
-            Color color = getColor(str);
-            if (color == null) {
-                error("Failed to parse rectangle color");
+        final String p1Str = "p1";
+        final String p2Str = "p2";
+        switch (nextOrder) {
+            case colorStr:
+                res = setColor(str, rectangle);
+                break;
+            case fillStr:
+                m = pFill.matcher(str);
+                if (m.find()) {
+                    rectangle.setFill("true".equals(m.group(1)));
+                } else {
+                    error("Failed to parse rectangle filled");
+                    res = -1;
+                }
+                break;
+            case p1Str:
+                m = pPoint.matcher(str);
+                if (m.find() && str.contains(p1Str)) {
+                    Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
+                    rectangle.setP1(point);
+                } else {
+                    error("Failed to parse rectangle p1");
+                    res = -1;
+                }
+                break;
+            case p2Str:
+                m = pPoint.matcher(str);
+                if (m.find() && str.contains(p2Str)) {
+                    Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
+                    rectangle.setP2(point);
+                } else {
+                    error("Failed to parse rectangle p2");
+                    res = -1;
+                }
+                break;
+            case endStr:
+                m = pRectangleEnd.matcher(str);
+                res = 0;
+                if (!m.matches()) {
+                    error("Failed to parse rectangle end");
+                    res = -1;
+                }
+                break;
+            default:
+                error("Failed to parse rectangle");
                 res = -1;
-            }
-            rectangle.setColor(color);
-        } else if (str.contains("filled:")) {
-            m = pFill.matcher(str);
-            if (m.find()) {
-                rectangle.setFill("true".equals(m.group(1)));
-            } else {
-                error("Failed to parse rectangle filled");
-                res = -1;
-            }
-        } else if (str.contains("p1:")) {
-            m = pPoint.matcher(str);
-            if (m.find()) {
-                Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
-                rectangle.setP1(point);
-            } else {
-                error("Failed to parse rectangle p1");
-                res = -1;
-            }
-        } else if (str.contains("p2:")) {
-            m = pPoint.matcher(str);
-            if (m.find()) {
-                Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
-                rectangle.setP2(point);
-            } else {
-                error("Failed to parse rectangle p2");
-                res = -1;
-            }
-        } else {
-            m = pRectangleEnd.matcher(str);
-            res = m.matches() ? 0 : -1;
+        }
+        if (res != -1) {
+            String[] strArr = {colorStr, fillStr, p1Str, p2Str, endStr};
+            setNextOrder(strArr);
         }
         return res;
     }
 
     /**
      * parse squiggle
-     * @param str       string input line str
+     *
+     * @param str      string input line str
      * @param squiggle SquiggleCommand
      * @return int 0 exist , -1 error , 1 continue
      */
     private int parseSquiggle(String str, SquiggleCommand squiggle) {
-        Matcher m;
+
         int res = 1;
-        if (str.contains("color:")) {
-            Color color = getColor(str);
-            if (color == null) {
-                error("Failed to parse squiggle color");
+        Matcher m;
+        final String pointsStr = "points";
+        final String pointStr = "point";
+        final String endPointStr = "endpoints";
+        switch (nextOrder) {
+            case colorStr:
+                res = setColor(str, squiggle);
+                break;
+            case fillStr:
+                m = pFill.matcher(str);
+                if (m.find()) {
+                    squiggle.setFill("true".equals(m.group(1)));
+                } else {
+                    error("Failed to parse squiggle filled");
+                    res = -1;
+                }
+                break;
+            case pointsStr:
+                if (this.points == null && squiggle.getPoints().size() == 0) {
+                    this.points = new ArrayList<>();
+                } else {
+                    error("Failed to parse squiggle points");
+                    res = -1;
+                }
+                break;
+            case pointStr:
+                m = pPoint.matcher(str);
+                if (m.find() && points != null) {
+                    Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
+                    points.add(point);
+                    nextOrder = pointsStr;
+                } else if (endPointStr.equals(str)) {
+                    for (Point p : points) {
+                        squiggle.add(p);
+                    }
+                    nextOrder = endPointStr;
+                    points = null;
+                } else {
+                    error("Failed to parse squiggle point:" + str);
+                    res = -1;
+                }
+                break;
+            case endStr:
+                m = pSquiggleEnd.matcher(str);
+                res = 0;
+                if (!m.matches()) {
+                    error("Failed to parse squiggle end");
+                    res = -1;
+                }
+                break;
+            default:
+                error("Failed to parse squiggle");
                 res = -1;
-            }
-            squiggle.setColor(color);
-        } else if (str.contains("filled:")) {
-            m = pFill.matcher(str);
-            if (m.find()) {
-                squiggle.setFill("true".equals(m.group(1)));
-            } else {
-                error("Failed to parse squiggle filled");
-                res = -1;
-            }
-        } else if ("points".equals(str)) {
-            if (this.points == null && squiggle.getPoints().size() == 0) {
-                this.points = new ArrayList<>();
-            } else {
-                error("Failed to parse squiggle points");
-                res = -1;
-            }
-        } else if (str.contains("point:")) {
-            m = pPoint.matcher(str);
-            if (m.find() && points != null) {
-                Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
-                points.add(point);
-            } else {
-                error("Failed to parse squiggle point");
-                res = -1;
-            }
-        } else if ("endpoints".equals(str)) {
-            for (Point p : points) {
-                squiggle.add(p);
-            }
-            points = null;
-        } else {
-            m = pSquiggleEnd.matcher(str);
-            res = m.matches() ? 0 : -1;
+        }
+        if (res != -1) {
+            String[] strArr = {colorStr, fillStr, pointsStr, pointStr, endPointStr, endStr};
+            setNextOrder(strArr);
         }
         return res;
     }
 
     /**
      * parse polyline
-     * @param str string input line str
+     *
+     * @param str      string input line str
      * @param polyline PolylineCommand
      * @return int 0 exist , -1 error , 1 continue
      */
     private int parsePolyline(String str, PolylineCommand polyline) {
-        Matcher m;
         int res = 1;
-        if (str.contains("color:")) {
-            Color color = getColor(str);
-            if (color == null) {
-                error("Failed to parse polyline color");
+        Matcher m;
+        final String pointsStr = "points";
+        final String pointStr = "point";
+        final String endPointStr = "endpoints";
+        switch (nextOrder) {
+            case colorStr:
+                res = setColor(str, polyline);
+                break;
+            case fillStr:
+                m = pFill.matcher(str);
+                if (m.find()) {
+                    polyline.setFill("true".equals(m.group(1)));
+                } else {
+                    error("Failed to parse polyline filled");
+                    res = -1;
+                }
+                break;
+            case pointsStr:
+                if (this.points == null && polyline.getPoints().size() == 0) {
+                    this.points = new ArrayList<>();
+                } else {
+                    error("Failed to parse polyline points");
+                    res = -1;
+                }
+                break;
+            case pointStr:
+                m = pPoint.matcher(str);
+                if (m.find() && points != null) {
+                    Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
+                    points.add(point);
+                    nextOrder = pointsStr;
+                } else if (endPointStr.equals(str)) {
+                    for (Point p : points) {
+                        polyline.add(p);
+                    }
+                    nextOrder = endPointStr;
+                    points = null;
+                } else {
+                    error("Failed to parse polyline point:" + str);
+                    res = -1;
+                }
+                break;
+            case endStr:
+                m = pPolylineEnd.matcher(str);
+                res = 0;
+                if (!m.matches()) {
+                    error("Failed to parse polyline end");
+                    res = -1;
+                }
+                break;
+            default:
+                error("Failed to parse squiggle");
                 res = -1;
-            }
-            polyline.setColor(color);
-        } else if (str.contains("filled:")) {
-            m = pFill.matcher(str);
-            if (m.find()) {
-                polyline.setFill("true".equals(m.group(1)));
-            } else {
-                error("Failed to parse polyline filled");
-                res = -1;
-            }
-        } else if ("points".equals(str)) {
-            if (this.points == null && polyline.getPoints().size() == 0) {
-                this.points = new ArrayList<>();
-            } else {
-                error("Failed to parse polyline points");
-                res = -1;
-            }
-        } else if (str.contains("point:")) {
-            m = pPoint.matcher(str);
-            if (m.find() && points != null) {
-                Point point = new Point(Integer.parseInt(m.group(2)), Integer.parseInt(m.group(3)));
-                points.add(point);
-            } else {
-                error("Failed to parse polyline point");
-                res = -1;
-            }
-        } else if ("endpoints".equals(str)) {
-            for (Point p : points) {
-                polyline.add(p);
-            }
-            points = null;
-        } else {
-            m = pPolylineEnd.matcher(str);
-            res = m.matches() ? 0 : -1;
+        }
+        if (res != -1) {
+            String[] strArr = {colorStr, fillStr, pointsStr, pointStr, endPointStr, endStr};
+            setNextOrder(strArr);
         }
         return res;
     }
 
     /**
      * get color by string
+     *
      * @param str string
      * @return Color
      */
@@ -393,12 +478,45 @@ public class PaintFileParser {
             int r = Integer.parseInt(m.group(1));
             int g = Integer.parseInt(m.group(2));
             int b = Integer.parseInt(m.group(3));
-            if (Math.max(Math.max(r, g), b) > 256) {
+            if (Math.max(Math.max(r, g), b) > 255) {
                 return null;
             }
             return Color.rgb(r, g, b);
         } else {
             return null;
+        }
+    }
+
+    /**
+     * set the paintCommand color
+     *
+     * @param str          color string
+     * @param paintCommand object
+     * @return 1 success,-1 error
+     */
+    private int setColor(String str, PaintCommand paintCommand) {
+        Color color = getColor(str);
+        int res = 1;
+        if (color == null) {
+            error("Failed to parse color");
+            res = -1;
+        }
+        paintCommand.setColor(color);
+        return res;
+    }
+
+    /**
+     * set the next string
+     *
+     * @param strArr orderList
+     */
+    private void setNextOrder(String[] strArr) {
+        for (int i = 0; i < strArr.length; i++) {
+            if (nextOrder.equals(strArr[i])) {
+                int index = (i + 1) % strArr.length;
+                nextOrder = strArr[index];
+                break;
+            }
         }
     }
 }
